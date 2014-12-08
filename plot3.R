9e5c6fe525ac1ab7e59b2ec100cc88bd0b85b36b
W1llD <dai.y@live.cn>
2014-12-08 09:41:07 +0800

    lty

--- plot3.R
+++ plot3.R
@@ -11,6 +11,6 @@ png(filename="plot3.png", width = 480, height = 480)
 plot(x$Time, x$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(x$Time, x$Sub_metering_2, type = "l", col = "red")
 lines(x$Time, x$Sub_metering_3, type = "l", col = "blue")
-legend("topright", lty = 3, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
 
--- plot4.R
+++ plot4.R
@@ -14,7 +14,7 @@ with(x,{
 	plot(Time, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 	lines(Time, Sub_metering_2, type = "l", col = "red")
 	lines(Time, Sub_metering_3, type = "l", col = "blue")
-	legend("topright", lty = 3, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+	legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 	plot(Time, Global_reactive_power, type = "l", xlab = "datetime")
 }) 
 dev.off()
